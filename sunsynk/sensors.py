189c9f2d82853e16ce82c0bcf28733bc67c76726
Johann Kellerman <kellerza@gmail.com>
2023-01-14 13:16:21 +0200

    abs

--- sunsynk/sensors.py
+++ sunsynk/sensors.py
@@ -84,6 +84,8 @@ class MathSensor(Sensor):
         self.value = int_round(
             sum(signed(i) * s for i, s in zip(self.reg_value, self.factors))
         )
+        if self.absolute and not isinstance(self.value, str) and self.value < 0:
+            self.value = -self.value
         if self.no_negative and not isinstance(self.value, str) and self.value < 0:
             self.value = 0
 
