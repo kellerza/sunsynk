ea6bc17fc7775a157aacde9c23d578d485106f8e
kababoom <mahagen@gmail.com>
2023-02-05 20:19:46 +0100

    Initial version of a definitions file for the 3phase inverters (#93)

--- sunsynk/definitions.py
+++ sunsynk/definitions.py
@@ -132,7 +132,7 @@ _SENSORS += (
     RATED_POWER,
     SERIAL,
     Sensor(0, "Device Type"),
-    FaultSensor((103, 104, 105, 106, 107), "Fault"),
+    FaultSensor((103, 104, 105, 106), "Fault"),
     InverterStateSensor(59, "Overall state"),
     SDStatusSensor(92, "SD Status", ""),  # type: ignore
     TempSensor(90, "DC transformer temperature", CELSIUS, 0.1),
