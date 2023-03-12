714b34ed11bfc31b421099b7075cbd7e5e6462bd
Johann Kellerman <kellerza@gmail.com>
2023-03-12 22:09:27 +0200

    0.3.0

--- sunsynk/__init__.py
+++ sunsynk/__init__.py
@@ -5,7 +5,7 @@ from typing import Final
 from .sensors import Sensor  # pylint: disable=unused-import
 from .sunsynk import Sunsynk  # pylint: disable=unused-import
 
-VERSION = "0.2.10"
+VERSION = "0.3.0"
 
 CELSIUS: Final = "°C"
 KWH: Final = "kWh"
