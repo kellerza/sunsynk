555fe5940f65d90292d796975aed09a6555c7cb7
xhanti <xhantibomela@gmail.com>
2023-06-05 20:40:05 +0200

    wip

--- sunsynk/solarmansunsynk.py
+++ sunsynk/solarmansunsynk.py
@@ -3,12 +3,15 @@ import logging
 import asyncio
 
 from sunsynk.sunsynk import Sunsynk
-from pysolarmanv5 import PySolarmanV5Async
+from pysolarmanv5 import PySolarmanV5Async, V5FrameError
 from typing import Sequence
 from urllib.parse import urlparse
 
 
 _LOGGER = logging.getLogger(__name__)
+
+
+retry_attempts = 5
 
 @attrs.define
 class SolarmanSunsynk(Sunsynk):  # pylint: disable=invalid-name
@@ -21,23 +24,24 @@ class SolarmanSunsynk(Sunsynk):  # pylint: disable=invalid-name
         url = urlparse(f"{self.port}")
         self.client = PySolarmanV5Async(
             address=url.hostname
-            ,serial=self.serial_nr
+            ,serial=int(self.serial_nr)
             ,port=url.port
             ,mb_slave_id=self.server_id
-            ,auto_reconnect=False
+            ,auto_reconnect=True
             ,verbose=False
-            ,socket_timeout=self.timeout)        
+            ,socket_timeout=60)
+        await self.client.connect()
 
     async def write_register(self, *, address: int, value: int) -> bool:
         """Write to a register - Sunsynk supports modbus function 0x10."""
         try:
-            await self.client.connect()
+            #await self.client.connect()
             await self.client.write_holding_register(address=address, value=value)
             return True            
         except asyncio.TimeoutError:
             _LOGGER.error("timeout writing register %s=%s", address, value)
-        finally:
-            await self.client.disconnect()
+        #finally:
+        #    await self.client.disconnect()
 
         self.timeouts += 1
         return False
@@ -45,9 +49,21 @@ class SolarmanSunsynk(Sunsynk):  # pylint: disable=invalid-name
     async def read_holding_registers(self, start: int, length: int) -> Sequence[int]:        
         """Read a holding register."""        
         try:
-            await self.client.connect()
+            #await self.client.connect()
             return await self.client.read_holding_registers(start, length)
         except asyncio.TimeoutError:
             raise IOError(f"failed to read register {start}")
-        finally:
-            await self.client.disconnect()        
+        except V5FrameError:
+            _LOGGER.error("frame error reading registers starting at: %s", start)
+            for attempt in range(retry_attempts):
+                try:                    
+                    return await self.client.read_holding_registers(start, length)
+                    #break
+                except Exception as e:                    
+                    _LOGGER.error("Caught error: %s", e)
+                    await asyncio.sleep(10)
+                    continue               
+            else:
+                raise IOError(f"failed to read register {start}")
+        #finally:
+        #   await self.client.disconnect()        
