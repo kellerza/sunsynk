851a076d9f68f5e89b5d5b6506e703826aab6d75
Johann Kellerman <kellerza@gmail.com>
2023-09-07 21:01:36 +0200

    edge

--- scripts/diff-dev.cmd
+++ scripts/diff-dev.cmd
@@ -1,11 +1,1 @@
-@echo off
-set a=hass-addon-sunsynk
-set d=hass-addon-sunsynk-dev
-git diff --no-index %a%\mqtt.py %d%\mqtt.py
-git diff --no-index %a%\filter.py %d%\filter.py
-git diff --no-index %a%\profiles.py %d%\profiles.py
-git diff --no-index %a%\options.py %d%\options.py
-git diff --no-index %a%\run.py %d%\run.py
-git diff --no-index %a%\config.yaml %d%\config.yaml
-git diff --no-index %a%\DOCS.md %d%\DOCS.md
-git diff --no-index %a%\Dockerfile %d%\Dockerfile
+git diff --no-index hass-addon-sunsynk-edge hass-addon-sunsynk-multi
