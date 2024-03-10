e94112e97e9df0805fe2bf0afb38f2f73bda85e9
Johann Kellerman <kellerza@gmail.com>
2024-03-10 17:17:09 +0200

    check definitions

--- src/sunsynk/definitions3phlv.py
+++ src/sunsynk/definitions3phlv.py
@@ -1,11 +1,17 @@
 """Sunsynk/Deye 5kW&8kW hybrid 3-phase LV inverter sensor definitions."""
 # pylint: disable=duplicate-code
 from sunsynk.definitions3ph import SENSORS
-from sunsynk.sensors import BinarySensor
 
 SENSORS = SENSORS.copy()
 
 ##########
 # General
 ##########
-SENSORS += (BinarySensor(194, "Grid Connected", bitmask=1 << 2),)
+
+
+SENSORS.deprecated.update(
+    {
+        "grid_voltage": "grid_l1_voltage",
+        "battery_activate": "battery_wake_up",
+    }
+)
