91ce12bb5393853631556c462bc6abfe6bdba3eb
maslyankov <m.maslyankov@me.com>
2024-12-28 16:45:05 +0200

    Fix double trailing slash on sitemap and handle this for future

--- www/docs/.vitepress/build_sitemap.js
+++ www/docs/.vitepress/build_sitemap.js
@@ -2,11 +2,14 @@ import {resolveOptions, generateSitemap} from "sitemap-ts";
 
 function buildEnd() {
   const opt = resolveOptions({
-    hostname: "https://sunsynk.wectrl.net/",
+    hostname: "https://sunsynk.wectrl.net",
     basePath: "/",
     outDir: "docs/.vitepress/dist",
     exclude: ["/404"],
     readable: true,
+    transformURL: (url) => {
+      return url.replace(/([^:])\/+/g, '$1/');
+    }
   });
 
   generateSitemap(opt);
