07fe3de381f146311283a88cd43838841f2a1307
maslyankov <m.maslyankov@me.com>
2024-12-28 23:26:04 +0200

    Remove basepath slash from sitemap config

--- www/docs/.vitepress/build_sitemap.js
+++ www/docs/.vitepress/build_sitemap.js
@@ -3,7 +3,7 @@ import {resolveOptions, generateSitemap} from "sitemap-ts";
 function buildEnd() {
   const opt = resolveOptions({
     hostname: "https://sunsynk.wectrl.net",
-    basePath: "/",
+    basePath: "",
     outDir: "docs/.vitepress/dist",
     exclude: ["/404"],
     readable: true,
