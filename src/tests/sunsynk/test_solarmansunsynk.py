09b4be3b7ed1037b0d052fd1be35ecbd0448346c
maslyankov <m.maslyankov@me.com>
2025-01-23 11:18:00 +0200

    Revert changes from pr 408

--- src/tests/sunsynk/test_solarmansunsynk.py
+++ src/tests/sunsynk/test_solarmansunsynk.py
@@ -2,15 +2,21 @@
 
 from typing import Any
 from unittest.mock import AsyncMock, patch
+from unittest.mock import AsyncMock, patch
 
 import pytest
 
 from sunsynk.solarmansunsynk import SolarmanSunsynk
 
 P_CONNECT = "sunsynk.solarmansunsynk.SolarmanSunsynk.connect"
+from sunsynk.solarmansunsynk import SolarmanSunsynk
+
+P_CONNECT = "sunsynk.solarmansunsynk.SolarmanSunsynk.connect"
 
 
 @pytest.mark.asyncio
+@patch(P_CONNECT, new_callable=AsyncMock)
+async def test_uss_sensor(connect: Any) -> None:
 @patch(P_CONNECT, new_callable=AsyncMock)
 async def test_uss_sensor(connect: Any) -> None:
     ss = SolarmanSunsynk(port="tcp://127.0.0.1:502", dongle_serial_number=101)
@@ -22,7 +28,17 @@ async def test_uss_sensor(connect: Any) -> None:
     assert not rhr.called
     await ss.read_holding_registers(1, 2)
     assert rhr.called
+    # await ss.connect()
+    ss.client = AsyncMock()
+    rhr = ss.client.read_holding_registers = AsyncMock()
 
+    # _LOGGER.warning("%s", dir(ss.client))
+    assert not rhr.called
+    await ss.read_holding_registers(1, 2)
+    assert rhr.called
+
+    wrr = ss.client.write_multiple_holding_registers = AsyncMock()
+    assert not wrr.called
     wrr = ss.client.write_multiple_holding_registers = AsyncMock()
     assert not wrr.called
     await ss.write_register(address=1, value=2)
