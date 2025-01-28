e53cf68709d2d9e55761218127848f67cbc4fe27
deondti <139332987+deondti@users.noreply.github.com>
2025-01-28 13:45:04 +0200

    Update single_phase_16kw.py

--- src/sunsynk/definitions/single_phase_16kw.py
+++ src/sunsynk/definitions/single_phase_16kw.py
@@ -2,18 +2,15 @@
 
 from sunsynk import AMPS, WATT
 from sunsynk.definitions.single_phase import SENSORS as COMMON
-from sunsynk.rwsensors import (
-    NumberRWSensor,
-)
-from sunsynk.sensors import (
-    Sensor,
-)
+from sunsynk.rwsensors import NumberRWSensor
+from sunsynk.sensors import Sensor
 
 SENSORS = COMMON.copy()
 
 #######
 # Grid
 #######
+
 SENSORS += (
     Sensor(169, "Grid power", WATT, -10),  # L1(167) + L2(168)
     Sensor(172, "Grid CT power", WATT, -10),
@@ -22,6 +19,7 @@ SENSORS += (
 #############
 # Inverter settings
 #############
+
 SENSORS += NumberRWSensor(230, "Grid Charge Battery current", AMPS, min=0, max=140)
 SENSORS += NumberRWSensor(210, "Battery Max Charge current", AMPS, min=0, max=290)
 SENSORS += NumberRWSensor(211, "Battery Max Discharge current", AMPS, min=0, max=290)
