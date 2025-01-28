ca730030afd8de397af9398d343bb1894cd653cb
deondti <139332987+deondti@users.noreply.github.com>
2025-01-28 15:58:49 +0200

    Create single_phase_16kw.py (#405)

---
+++ src/sunsynk/definitions/single_phase_16kw.py
@@ -0,0 +1,25 @@
+"""Sunsynk / Deye 16kW hybrid inverter sensor definitions."""
+
+from sunsynk import AMPS, WATT
+from sunsynk.definitions.single_phase import SENSORS as COMMON
+from sunsynk.rwsensors import NumberRWSensor
+from sunsynk.sensors import Sensor
+
+SENSORS = COMMON.copy()
+
+#######
+# Grid
+#######
+
+SENSORS += (
+    Sensor(169, "Grid power", WATT, -10),  # L1(167) + L2(168)
+    Sensor(172, "Grid CT power", WATT, -10),
+)
+
+#############
+# Inverter settings
+#############
+
+SENSORS += NumberRWSensor(230, "Grid Charge Battery current", AMPS, min=0, max=140)
+SENSORS += NumberRWSensor(210, "Battery Max Charge current", AMPS, min=0, max=290)
+SENSORS += NumberRWSensor(211, "Battery Max Discharge current", AMPS, min=0, max=290)
