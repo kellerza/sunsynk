f755ee3c914dba6bbae9d032916e58d6f80bed21
Johann Kellerman <kellerza@gmail.com>
2025-02-21 10:07:28 +0200

    dockerfile

--- scripts/copy2local.cmd
+++ scripts/copy2local.cmd
@@ -6,35 +6,21 @@ rem xcopy /Y hass-addon-mbusd \\192.168.1.7\addons\hass-addon-mbusd\
 EXIT /B %ERRORLEVEL%
 
 :copy2
-echo # Copy '%~1' to '%~2'
-xcopy /Y %~1 %~2
-xcopy /Y setup.* %~2\sunsynk\
-xcopy /Y README.md %~2\sunsynk\
-xcopy /Y /S %~1\rootfs %~2\rootfs\
-xcopy /Y /S %~1\translations %~2\translations\
-xcopy /Y /S /EXCLUDE:scripts\copyexclude.txt src %~2\sunsynk\src\
+echo # Copy local Dockerfile
+xcopy /Y %~1\Dockerfile.local %~2\Dockerfile
 
-for /f %%i in ('application arg0 arg1') do set ID=%%i
-echo %ID%
 echo # Modify Config for local testing
 set cf=%~1\config.localtest.yaml
 cp %~1\config.yaml %cf%
 sed -i 's/image:/# image:/' %cf%
 sed -i 's/name: /name: LOCAL /' %cf%
-@REM echo %TIME%| sed -r 's/([0-9]+:)+//'> .id
-@REM set /p "VER=" < .id
-@REM echo Version: "%VER%"
-@REM sed -i 's/version:.*\"/version: \"%VER%\"/' %cf%
 xcopy /Y %cf% %~2\config.yaml
 
-@REM echo # Modify Dockerfile
-@REM cp %~1\Dockerfile %~1\Dockerfile.local
-@REM rem Comment out installing sunsynk from pypi
-@REM sed -i 's/RUN pip3/# RUN pip3/' %~1\Dockerfile.local
-@REM rem Uncomment local test commands
-@REM sed -i -E 's/#! (# )?//' %~1\Dockerfile.local
-@REM rem sed -i 's/#! //' %~1\Dockerfile.local
-@REM xcopy /Y %~1\Dockerfile.local %~2\Dockerfile
-xcopy /Y %~1\Dockerfile.local %~2\Dockerfile
+echo # Copy '%~1' to '%~2'
+xcopy /Y /S /EXCLUDE:scripts\copyexclude.txt %~1 %~2
+
+echo # Copy sunsynk package
+for %%f in (pyproject.toml,MANIFEST.in,LICENSE,README.md,uv.lock) do xcopy /Y "%%f" %~2\sunsynk\
+xcopy /Y /S /EXCLUDE:scripts\copyexclude.txt src %~2\sunsynk\src\
 
 EXIT /B 0
