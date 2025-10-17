6eed2b419ff8e4a8edbfcc5e99ec3da15742a648
Johann Kellerman <kellerza@gmail.com>
2025-10-17 12:00:08 +0200

    mbusd

--- scripts/copy2local.cmd
+++ scripts/copy2local.cmd
@@ -1,9 +1,17 @@
 @echo off
-CALL :copy2 hass-addon-sunsynk-multi,\\192.168.1.8\addons\hass-addon-sunsynk-multi\
+@rem CALL :copy_ss hass-addon-sunsynk-multi,\\192.168.1.8\addons\hass-addon-sunsynk-multi
+CALL :copy_addon hass-addon-mbusd,\\192.168.1.8\addons\hass-addon-mbusd
 
 EXIT /B %ERRORLEVEL%
 
-:copy2
+:copy_ss
+
+echo # Copy sunsynk package
+for %%f in (pyproject.toml,MANIFEST.in,LICENSE,README.md,uv.lock) do xcopy /Y "%%f" %~2\sunsynk\
+xcopy /Y /S /EXCLUDE:scripts\copyexclude.txt src %~2\sunsynk\src\
+
+
+:copy_addon
 echo # Modify Config for local testing
 set cf=%~1\config.localtest.yaml
 cp %~1\config.yaml %cf%
@@ -13,9 +21,5 @@ xcopy /Y %cf% %~2\config.yaml
 
 echo # Copy '%~1' to '%~2'
 xcopy /Y /S /EXCLUDE:scripts\copyexclude.txt %~1 %~2
-
-echo # Copy sunsynk package
-for %%f in (pyproject.toml,MANIFEST.in,LICENSE,README.md,uv.lock) do xcopy /Y "%%f" %~2\sunsynk\
-xcopy /Y /S /EXCLUDE:scripts\copyexclude.txt src %~2\sunsynk\src\
 
 EXIT /B 0
