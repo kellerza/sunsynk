a0834cfda9b66e7d7e6e0f9a246e0e28f0dcd413
Johann Kellerman <kellerza@gmail.com>
2026-02-11 08:39:56 +0200

    Deps & scripts

--- scripts/copy2local.cmd
+++ scripts/copy2local.cmd
@@ -1,26 +1,43 @@
 @echo off
-CALL :copy_ss hass-addon-sunsynk-multi,\\192.168.1.8\addons\hass-addon-sunsynk-multi
-CALL :copy_addon hass-addon-mbusd,\\192.168.1.8\addons\hass-addon-mbusd
+set DEST=\\192.168.1.8\addons
+CALL :copy_sunsynk hass-addon-sunsynk-edge
+CALL :copy_addon hass-addon-sunsynk-edge
+CALL :copy_builder hass-addon-sunsynk-edge
+
+rem CALL :copy_config hass-addon-mbusd,\\192.168.1.8\addons\hass-addon-mbusd
+rem CALL :copy__addon hass-addon-mbusd,\\192.168.1.8\addons\hass-addon-mbusd
 
 EXIT /B %ERRORLEVEL%
 
-:copy_ss
-
-echo # Copy sunsynk package
-for %%f in (pyproject.toml,MANIFEST.in,LICENSE,README.md,uv.lock) do xcopy /Y "%%f" %~2\sunsynk\
-xcopy /Y /S /EXCLUDE:scripts\copyexclude.txt src %~2\sunsynk\src\
-
+:copy_sunsynk
+CALL :print "Copy sunsynk package for '%~1'"
+for %%f in (pyproject.toml,MANIFEST.in,LICENSE,README.md,uv.lock) do xcopy /Y "%%f" %DEST%\%~1\sunsynk\
+xcopy /Y /S /EXCLUDE:scripts\copyexclude.txt src %DEST%\%~1\sunsynk\src\
 
 :copy_addon
-echo # Modify Config for local testing
+CALL :print "Copy '%~1' to '%DEST%\%~1'"
+xcopy /Y /S /EXCLUDE:scripts\copyexclude.txt %~1 %DEST%\%~1
+EXIT /B 0
+
+CALL :print "Modify Config for local testing"
 set cf=%~1\config.localtest.yaml
 cp %~1\config.yaml %cf%
 sed -i 's/image:/# image:/' %cf%
 sed -i 's/name: /name: A_LOCAL /' %cf%
-xcopy /Y %cf% %~2\config.yaml
-echo 0.0.0 > %~1\VERSION
+xcopy /Y %cf% %DEST%\%~1\config.yaml*
+EXIT /B 0
 
-echo # Copy '%~1' to '%~2'
-xcopy /Y /S /EXCLUDE:scripts\copyexclude.txt %~1 %~2
+:copy_builder
+CALL :print "Copy builder files for '%~1'"
+xcopy /Y hass-addon-sunsynk-multi\Dockerfile %DEST%\%~1\
+xcopy /Y hass-addon-sunsynk-multi\build.yaml %DEST%\%~1\
+xcopy /Y /S hass-addon-sunsynk-multi\rootfs %DEST%\%~1\rootfs\
+echo 0.0.0 > %DEST%\%~1\VERSION
+EXIT /B 0
 
+:print
+echo.
+echo %~1
+echo ===========================================================
+echo.
 EXIT /B 0
